3358718e067dc0bcc3ca07643528f375b7afd913
James Acosta <jamesacosta0531@gmail.com>
2021-03-12 20:17:20 -0500

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -13,11 +13,28 @@ public class PlayerScript : MonoBehaviour
 
     private int scoreValue = 0;
 
+    public Text winText;
+
+    public Text livesText;
+
+    private int Lives = 3;
+
+    public AudioClip BackgroundMusic;
+
+    public AudioClip WinMusic;
+
+    public AudioSource musicSource; 
+
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        winText.text = "";
+        livesText.text = Lives.ToString();
+        musicSource.clip = BackgroundMusic;
+        musicSource.Play();
+        musicSource.loop = true;
     }
 
     // Update is called once per frame
@@ -39,8 +56,33 @@ public class PlayerScript : MonoBehaviour
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
-        }
 
+            if (scoreValue == 4)
+            {
+                transform.position = new Vector3(74.0f, 1.0f, 0.0f);
+                Lives = 3;
+                livesText.text = Lives.ToString();
+            }
+        }
+        if (scoreValue >= 8)
+        {
+            winText.text = "You win! Game created by James Acosta.";
+            musicSource.Stop();
+            musicSource.clip = WinMusic;
+            musicSource.Play();
+            musicSource.loop = true;
+        }
+        if (collision.collider.tag == "Enemy")
+        {
+            Lives -= 1;
+            livesText.text = Lives.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+        if (Lives == 0)
+        {
+            winText.text = "You Lose! Game created by James Acosta.";
+            Destroy(this.gameObject);
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
